dd6849d93cf9c18fcf10ea7fc52becc350bd75a0
YvonneAriri <aririyvonne04@gmail.com>
2023-10-14 01:36:25 -0500

    Implemented the API

--- Trivia/TriviaQuestion.swift
+++ Trivia/TriviaQuestion.swift
@@ -7,9 +7,65 @@
 
 import Foundation
 
-struct TriviaQuestion {
-  let category: String
-  let question: String
-  let correctAnswer: String
-  let incorrectAnswers: [String]
+struct TriviaAPIResponse: Decodable {
+    let results: [TriviaQuestion]
+}
+
+struct TriviaQuestion: Decodable {
+    let category: String
+    let question: String
+    let correctAnswer: String
+    let incorrectAnswers: [String]
+
+    private enum CodingKeys: String, CodingKey {
+        case category = "category"
+        case question = "question"
+        case correctAnswer = "correct_answer"
+        case incorrectAnswers = "incorrect_answers" 
+    }
+}
+
+class TriviaService {
+    static func fetchTriviaQuestions(completion: ((Result<[TriviaQuestion], Error>) -> Void)? = nil) {
+        let url = URL(string: "https://opentdb.com/api.php?amount=20")!
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion?(.failure(error))
+                }
+                return
+            }
+
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                let statusCode = (response as? HTTPURLResponse)?.statusCode ?? -1
+                let error = NSError(domain: "HTTPError", code: statusCode, userInfo: nil)
+                DispatchQueue.main.async {
+                    completion?(.failure(error))
+                }
+                return
+            }
+
+            guard let data = data else {
+                let error = NSError(domain: "NoDataError", code: -1, userInfo: nil)
+                DispatchQueue.main.async {
+                    completion?(.failure(error))
+                }
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(TriviaAPIResponse.self, from: data)
+                DispatchQueue.main.async {
+                    completion?(.success(response.results))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion?(.failure(error))
+                }
+            }
+        }
+        task.resume()
+    }
+    
 }
--- Trivia/TriviaViewController.swift
+++ Trivia/TriviaViewController.swift
@@ -25,32 +25,61 @@ class TriviaViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     addGradient()
+ 
     questionContainerView.layer.cornerRadius = 8.0
-    // TODO: FETCH TRIVIA QUESTIONS HERE
+      TriviaService.fetchTriviaQuestions { [weak self] result in
+          guard let self = self else { return }
+
+          switch result {
+          case .success(let questions):
+              self.questions = questions
+              self.updateQuestion(withQuestionIndex: 0)
+          case .failure(let error):
+              let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+              let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+              alertController.addAction(okAction)
+              self.present(alertController, animated: true, completion: nil)
+          }
+      }
   }
   
-  private func updateQuestion(withQuestionIndex questionIndex: Int) {
-    currentQuestionNumberLabel.text = "Question: \(questionIndex + 1)/\(questions.count)"
-    let question = questions[questionIndex]
-    questionLabel.text = question.question
-    categoryLabel.text = question.category
-    let answers = ([question.correctAnswer] + question.incorrectAnswers).shuffled()
-    if answers.count > 0 {
-      answerButton0.setTitle(answers[0], for: .normal)
+    private func updateQuestion(withQuestionIndex questionIndex: Int) {
+        currentQuestionNumberLabel.text = "Question: \(questionIndex + 1)/\(questions.count)"
+        let question = questions[questionIndex]
+        questionLabel.text = question.question
+        categoryLabel.text = question.category
+
+        if question.correctAnswer == "True" && question.incorrectAnswers.contains("False") {
+                answerButton0.setTitle("True", for: .normal)
+                answerButton1.setTitle("False", for: .normal)
+                answerButton1.isHidden = false
+                answerButton2.isHidden = true
+                answerButton3.isHidden = true
+            } else if question.correctAnswer == "False" && question.incorrectAnswers.contains("True") {
+                answerButton0.setTitle("False", for: .normal)
+                answerButton1.setTitle("True", for: .normal)
+                answerButton1.isHidden = false
+                answerButton2.isHidden = true
+                answerButton3.isHidden = true
+        } else {
+            let answers = ([question.correctAnswer] + question.incorrectAnswers).shuffled()
+            if answers.count > 0 {
+                answerButton0.setTitle(answers[0], for: .normal)
+            }
+            if answers.count > 1 {
+                answerButton1.setTitle(answers[1], for: .normal)
+                answerButton1.isHidden = false
+            }
+            if answers.count > 2 {
+                answerButton2.setTitle(answers[2], for: .normal)
+                answerButton2.isHidden = false
+            }
+            if answers.count > 3 {
+                answerButton3.setTitle(answers[3], for: .normal)
+                answerButton3.isHidden = false
+            }
+        }
     }
-    if answers.count > 1 {
-      answerButton1.setTitle(answers[1], for: .normal)
-      answerButton1.isHidden = false
-    }
-    if answers.count > 2 {
-      answerButton2.setTitle(answers[2], for: .normal)
-      answerButton2.isHidden = false
-    }
-    if answers.count > 3 {
-      answerButton3.setTitle(answers[3], for: .normal)
-      answerButton3.isHidden = false
-    }
-  }
   
   private func updateToNextQuestion(answer: String) {
     if isCorrectAnswer(answer) {
@@ -91,6 +120,7 @@ class TriviaViewController: UIViewController {
     view.layer.insertSublayer(gradientLayer, at: 0)
   }
   
+    
   @IBAction func didTapAnswerButton0(_ sender: UIButton) {
     updateToNextQuestion(answer: sender.titleLabel?.text ?? "")
   }
@@ -106,5 +136,25 @@ class TriviaViewController: UIViewController {
   @IBAction func didTapAnswerButton3(_ sender: UIButton) {
     updateToNextQuestion(answer: sender.titleLabel?.text ?? "")
   }
+    
+    @IBAction func didTapResetButton(_ sender: UIButton) {
+        questions = []
+        currQuestionIndex = 0
+        numCorrectQuestions = 0
+
+        TriviaService.fetchTriviaQuestions { [weak self] result in
+            guard let self = self else { return }
+
+            switch result {
+            case .success(let newQuestions):
+                self.questions = newQuestions
+                self.updateQuestion(withQuestionIndex: 0)
+
+            case .failure(let error):
+                print("Error fetching new questions: \(error)")
+        }
+    }
+    }
 }
+
 
